efd89a5463e1e69e9fa1eb42320d5e59004a0122
Caesar Garcia <investingalchemy@proton.me>
2025-02-06 00:44:10 -0800

    added 3 python files from the daily excercies of day 2

---
+++ Total_grocery_cost.py
@@ -0,0 +1,11 @@
+#price of grocery items
+milk = 12.50
+bread = 3.69
+eggs = 5.99
+
+#calculating total cost
+total_cost = milk + bread + eggs
+
+#Print total cost
+print("The total is: $" + str(total_cost))
+
--- my_first_script.py
+++ my_first_script.py
@@ -1,1 +1,20 @@
-print("Hello World!")
+#print("Hello World!")
+
+#price of grocery items
+milk = 12.50
+bread = 3.69
+eggs = 5.99
+
+#calculating total cost
+total_cost = milk + bread + eggs
+
+#Print total cost
+print("The total is: $" + str(total_cost))
+
+
+#Asking for Favorite grocery store
+Favorite_Grocery_Store = input("What is your favorite grocery store?")
+
+#Greeting them to store
+print(f"Welcome to {Favorite_Grocery_Store}")
+
