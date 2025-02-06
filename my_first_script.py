f283c34672b847de4a4af0d6486f77158070a303
Caesar Garcia <investingalchemy@proton.me>
2025-02-06 00:51:42 -0800

    updated files from day 2

--- my_first_script.py
+++ my_first_script.py
@@ -1,20 +1,7 @@
 #print("Hello World!")
 
-#price of grocery items
-milk = 12.50
-bread = 3.69
-eggs = 5.99
-
-#calculating total cost
-total_cost = milk + bread + eggs
-
-#Print total cost
-print("The total is: $" + str(total_cost))
 
 
-#Asking for Favorite grocery store
-Favorite_Grocery_Store = input("What is your favorite grocery store?")
 
-#Greeting them to store
-print(f"Welcome to {Favorite_Grocery_Store}")
+
 
